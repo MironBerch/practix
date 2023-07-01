10034e5455cd60d42aafbec6e1f94e887d6c3e6e
MironBerch <mironberchetov@gmail.com>
2023-07-01 22:25:25 +0300

    Delete `notifications`

--- images/notifications/receiver/src/api/urls.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from api.handlers import router
-
-api_router = APIRouter(prefix='/api')
-
-# Notifications
-api_router.include_router(router=router)
--- images/notifications/receiver/src/db/rabbitmq.py
+++
@@ -1,7 +0,0 @@
-import aio_pika
-
-rabbitmq: aio_pika.Connection | None = None
-
-
-async def get_rabbitmq():
-    return rabbitmq
