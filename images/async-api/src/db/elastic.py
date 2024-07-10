e7a81aa95b25a33ca178308c83cbfe891c15f816
MironBerch <mironberchetov@gmail.com>
2024-07-10 20:50:47 +0300

    Create elasticsearch and redis connections for `async-api`

---
+++ images/async-api/src/db/elastic.py
@@ -0,0 +1,130 @@
+import logging
+
+from elasticsearch import AsyncElasticsearch, RequestError
+
+logger = logging.getLogger()
+
+elastic: AsyncElasticsearch | None = None
+
+
+SETTINGS = {
+    'refresh_interval': '1s',
+    'analysis': {
+        'filter': {
+            'english_stop': {
+                'type': 'stop',
+                'stopwords': '_english_',
+            },
+            'english_stemmer': {
+                'type': 'stemmer',
+                'language': 'english',
+            },
+            'english_possessive_stemmer': {
+                'type': 'stemmer',
+                'language': 'possessive_english',
+            },
+            'russian_stop': {
+                'type': 'stop',
+                'stopwords': '_russian_',
+            },
+            'russian_stemmer': {
+                'type': 'stemmer',
+                'language': 'russian',
+            },
+        },
+        'analyzer': {
+            'ru_en': {
+                'tokenizer': 'standard',
+                'filter': [
+                    'lowercase',
+                    'english_stop',
+                    'english_stemmer',
+                    'english_possessive_stemmer',
+                    'russian_stop',
+                    'russian_stemmer',
+                ],
+            },
+        },
+    },
+}
+
+INDICES = {
+    'movies': {
+        'id': {'type': 'keyword'},
+        'rating': {'type': 'float'},
+        'genres': {'type': 'keyword'},
+        'title': {
+            'type': 'text',
+            'analyzer': 'ru_en',
+            'fields': {'raw': {'type': 'keyword'}},
+        },
+        'description': {'type': 'text', 'analyzer': 'ru_en'},
+        'directors_names': {'type': 'text', 'analyzer': 'ru_en'},
+        'actors_names': {'type': 'text', 'analyzer': 'ru_en'},
+        'writers_names': {'type': 'text', 'analyzer': 'ru_en'},
+        'actors': {
+            'type': 'nested',
+            'dynamic': 'strict',
+            'properties': {
+                'id': {'type': 'keyword'},
+                'name': {'type': 'text', 'analyzer': 'ru_en'},
+            },
+        },
+        'writers': {
+            'type': 'nested',
+            'dynamic': 'strict',
+            'properties': {
+                'id': {'type': 'keyword'},
+                'name': {'type': 'text', 'analyzer': 'ru_en'},
+            },
+        },
+        'directors': {
+            'type': 'nested',
+            'dynamic': 'strict',
+            'properties': {
+                'id': {'type': 'keyword'},
+                'name': {'type': 'text', 'analyzer': 'ru_en'},
+            },
+        },
+    },
+    'persons': {
+        'id': {'type': 'keyword'},
+        'full_name': {
+            'type': 'text',
+            'analyzer': 'ru_en',
+            'fields': {'raw': {'type': 'keyword'}},
+        },
+    },
+    'genres': {
+        'id': {'type': 'keyword'},
+        'name': {
+            'type': 'text',
+            'analyzer': 'ru_en',
+            'fields': {'raw': {'type': 'keyword'}},
+        },
+        'description': {'type': 'text', 'analyzer': 'ru_en'},
+    },
+}
+
+
+async def get_elastic() -> AsyncElasticsearch | None:
+    return elastic
+
+
+async def create_indexes() -> None:
+    """Создаёт индексы с соответствующими настройками и схемами данных."""
+    try:
+        for index, properties in INDICES.items():
+            if not await elastic.indices.exists(index=index):
+                body = {
+                    'settings': SETTINGS,
+                    'mappings': {
+                        'dynamic': 'strict',
+                        'properties': properties,
+                    },
+                }
+                await elastic.indices.create(index=index, body=body)
+    except RequestError as exc:
+        logger.error(exc)
+    else:
+        logger.info('Elasticsearch indexes created successfully')
