d3fa6d3dd0fabea41c52d1b1da593f4def63af80
MironBerch <mironberchetov@gmail.com>
2024-07-19 23:41:55 +0300

    Create redis adapter for `async-api`

--- images/async-api/src/db/redis.py
+++ images/async-api/src/db/redis.py
@@ -3,5 +3,24 @@ from redis import Redis
 redis: Redis | None = None
 
 
-async def get_redis() -> Redis:
-    return redis
+class RedisAdapter:
+    def __init__(self, redis_instance: Redis):
+        self.redis = redis_instance
+
+    async def get_objects_from_cache(self, redis_key: str) -> list | dict | None:
+        data = await self.redis.get(redis_key)
+        if not data:
+            return None
+        return data
+
+    async def put_objects_to_cache(
+            self,
+            redis_key: str,
+            objects: list | dict,
+            expire: int = 60 * 10,
+    ) -> None:
+        await self.redis.set(redis_key, objects, ex=expire)
+
+
+async def get_redis() -> RedisAdapter:
+    return RedisAdapter(redis_instance=redis)
