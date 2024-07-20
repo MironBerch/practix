153353d3940ef1dd54a27bdc362174076366a910
MironBerch <mironberchetov@gmail.com>
2024-07-20 23:01:43 +0300

    Create elasticsearch adapter for `async-api`

--- images/async-api/src/db/elastic.py
+++ images/async-api/src/db/elastic.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any
 
 from elasticsearch import AsyncElasticsearch, RequestError
 
@@ -128,3 +129,28 @@ async def create_indexes() -> None:
         logger.error(exc)
     else:
         logger.info('Elasticsearch indexes created successfully')
+
+
+class ElasticAdapter:
+    def __init__(self, elastic: AsyncElasticsearch):
+        self.elastic = elastic
+
+    async def get_objects_from_db(
+            self,
+            index: str,
+            model: Any,
+            query: dict,
+            page_size: int,
+            page_number: int,
+    ) -> list:
+        data = await self.elastic.search(
+            index=index,
+            body=query,
+            size=page_size,
+            from_=(page_number - 1) * page_size,
+        )
+        return [model(**document['_source']) for document in data['hits']['hits']]
+
+    async def get_object_from_db(self, index: str, model: Any, object_pk: str):
+        doc = await self.elastic.get(index, object_pk)
+        return model(**doc['_source'])
