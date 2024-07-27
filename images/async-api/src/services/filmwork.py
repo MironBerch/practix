a7d2ed9ee9c51a1a4fbbe5c6a81875028f93923d
MironBerch <mironberchetov@gmail.com>
2024-07-27 23:05:47 +0300

    Create filmwork services for `async-api`

---
+++ images/async-api/src/services/filmwork.py
@@ -0,0 +1,81 @@
+from fastapi import Depends
+
+from db.elastic import ElasticAdapter, get_elastic_adapter
+from db.redis import RedisAdapter, get_redis
+from models.filmwork import BaseFilmwork, Filmwork
+from services.base import BaseListService, RetrieveService
+
+
+class FilmworksListService(BaseListService):
+
+    @staticmethod
+    def get_elastic_query(genres: list[str] | None, sort: str | None) -> dict:
+        query = {
+            'query': {
+                'bool': {},
+            },
+            'sort': {},
+        }
+        if sort:
+            field = sort.lstrip('-')
+            query['sort'][field] = {'order': 'asc' if sort.startswith('-') else 'desc'}
+        if genres:
+            query['query']['bool']['must'] = [
+                {
+                    'terms': {
+                        'genres': genres,
+                    },
+                },
+            ]
+        return query
+
+
+class FilmworksSearchService(BaseListService):
+
+    @staticmethod
+    def get_elastic_query(query: str) -> dict:
+        query = {
+            'query': {
+                'multi_match': {
+                    'query': query,
+                    'fields': ['title', 'description'],
+                },
+            },
+        }
+        return query
+
+
+def get_filmworks_list_service(
+        redis: RedisAdapter = Depends(get_redis),
+        elastic: ElasticAdapter = Depends(get_elastic_adapter),
+) -> FilmworksListService:
+    return FilmworksListService(
+        cache_adapter=redis,
+        db_adapter=elastic,
+        index='movies',
+        model=BaseFilmwork,
+    )
+
+
+def get_filmworks_search_service(
+        redis: RedisAdapter = Depends(get_redis),
+        elastic: ElasticAdapter = Depends(get_elastic_adapter),
+) -> FilmworksSearchService:
+    return FilmworksSearchService(
+        cache_adapter=redis,
+        db_adapter=elastic,
+        index='movies',
+        model=BaseFilmwork,
+    )
+
+
+def get_retrieve_filmwork_service(
+        redis: RedisAdapter = Depends(get_redis),
+        elastic: ElasticAdapter = Depends(get_elastic_adapter),
+) -> RetrieveService:
+    return RetrieveService(
+        cache_adapter=redis,
+        db_adapter=elastic,
+        index='movies',
+        model=Filmwork,
+    )
