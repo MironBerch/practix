22a6068eabad762f6257726c67c0eb6631d1b340
MironBerch <mironberchetov@gmail.com>
2024-08-04 23:59:08 +0300

    Create postgres connection for `auth`

---
+++ images/auth/src/db/db.py
@@ -0,0 +1,23 @@
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from flask import Flask
+
+from core.config import settings
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def init(app: Flask):
+    app.settings['SQLALCHEMY_DATABASE_URI'] = (
+        'postgresql://{user}:{password}@{host}:{port}/{db}'.format(
+            user=settings.postgres.user,
+            password=settings.postgres.password,
+            host=settings.postgres.host,
+            port=settings.postgres.port,
+            db=settings.postgres.db,
+        )
+    )
+    db.init_app(app)
+    migrate.init_app(app, db)
