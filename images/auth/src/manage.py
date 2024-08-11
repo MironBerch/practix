97245e7ed78db78e4730ef98fa6fe2764956d2d1
MironBerch <mironberchetov@gmail.com>
2024-08-11 23:57:34 +0300

    Create flask cli commands for `auth`

--- images/auth/src/manage.py
+++ images/auth/src/manage.py
@@ -1,8 +1,30 @@
+import click
+import flask_migrate
+
 from flask import Flask
+from flask.cli import with_appcontext
 
 from core.config import settings
 from core.logger import logger
 from db import postgres, redis
+
+
+@click.command()
+@with_appcontext
+def makemigrations():
+    flask_migrate.migrate()
+
+
+@click.command()
+@with_appcontext
+def migrate():
+    flask_migrate.upgrade()
+
+
+@click.command()
+@with_appcontext
+def createsuperuser():
+    ...
 
 
 def create_app() -> Flask:
@@ -10,6 +32,9 @@ def create_app() -> Flask:
     app.logger = logger
     postgres.init(app)
     redis.redis = redis.init()
+    app.cli.add_command(makemigrations)
+    app.cli.add_command(migrate)
+    app.cli.add_command(createsuperuser)
     return app
 
 
@@ -18,5 +43,5 @@ if __name__ == '__main__':
     app.run(
         host=settings.flask.host,
         port=settings.flask.port,
-        debug=settings.flask.debug,
+        debug=settings.flask.debug
     )
