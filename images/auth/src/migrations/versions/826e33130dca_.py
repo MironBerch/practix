03781a75728f219473286620f0930485baa162ca
MironBerch <mironberchetov@gmail.com>
2024-10-09 00:39:30 +0300

    Create session table partitions and make new migration for `auth`

--- images/auth/src/migrations/versions/826e33130dca_.py
+++ images/auth/src/migrations/versions/826e33130dca_.py
@@ -5,9 +5,8 @@ Revises:
 Create Date: 2024-10-08 21:16:02.356043
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = '826e33130dca'
