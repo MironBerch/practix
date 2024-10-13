60530bd45413114a6fd8ec1c663f4b6ba2e91e79
MironBerch <mironberchetov@gmail.com>
2024-10-13 18:17:58 +0300

    Register user routers for `auth`

--- images/auth/src/api/urls.py
+++ images/auth/src/api/urls.py
@@ -1,9 +1,10 @@
 from flask import Flask
 
-from api.v1 import auth, health
+from api.v1 import auth, health, user
 
 
 def init_routers(app: Flask) -> None:
     """Register all `flask.Blueprint` on the application."""
     app.register_blueprint(health.bp)
     app.register_blueprint(auth.bp)
+    app.register_blueprint(user.bp)
