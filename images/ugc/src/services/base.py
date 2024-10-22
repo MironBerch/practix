baf0d4c7502fde47e1ae6d7bb15d92c5041de9e7
MironBerch <mironberchetov@gmail.com>
2024-10-22 20:27:33 +0300

    Create base service for `ugc`

---
+++ images/ugc/src/services/base.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+from motor.motor_asyncio import AsyncIOMotorDatabase
+
+
+class BaseService(ABC):
+    def __init__(self, mongo: AsyncIOMotorDatabase):
+        self.mongo = mongo
+
+    @abstractmethod
+    def get(self):
+        pass
+
+    @abstractmethod
+    def update(self):
+        pass
+
+    @abstractmethod
+    def remove(self):
+        pass
