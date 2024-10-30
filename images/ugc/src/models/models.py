def509fd822d553929581b6fa90bbd19ac2eb653
MironBerch <mironberchetov@gmail.com>
2024-10-30 22:36:58 +0300

    Create review model for `ugc`

--- images/ugc/src/models/models.py
+++ images/ugc/src/models/models.py
@@ -1,9 +1,22 @@
+from datetime import datetime
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class FilmworkBookmark(BaseModel):
     """Модель ответа для представления закладки."""
 
     filmwork_id: UUID
+
+
+class Review(BaseModel):
+    """Модель ответа для представления рецензии на фильм."""
+
+    id: UUID
+    author_id: UUID
+    filmwork_id: UUID
+    text: str
+    pub_date: datetime
+    likes: int = Field(default=0)
+    dislikes: int = Field(default=0)
