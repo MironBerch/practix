b53971fb592e0cc00ae80b9ad817a66e47f6be8a
MironBerch <mironberchetov@gmail.com>
2024-10-31 16:56:29 +0300

    Update filmworks service for `ugc`

--- images/ugc/src/services/filmworks.py
+++ images/ugc/src/services/filmworks.py
@@ -11,6 +11,43 @@ class FilmworksService(BaseService):
     async def get(self, filmwork_id: UUID | str):
         return self.mongo['filmworks'].find_one({'_id': filmwork_id})
 
+    async def get_rating(self, filmwork_id: UUID | str):
+        filmwork = await self.mongo['filmworks'].find_one({'_id': filmwork_id})
+        return filmwork['rating']
+
+    async def rate(
+            self,
+            filmwork_id: UUID | str,
+            user_id: UUID | str,
+            score: int | float,
+    ):
+        filmwork: dict = await self.mongo['filmworks'].find_one(
+            {'_id': filmwork_id},
+        )
+        votes: list = filmwork.get('rating', {}).get('votes', [])
+        for vote in votes:
+            if vote['user_id'] == user_id:
+                vote['score'] = score
+                break
+        else:
+            votes.append({'user_id': user_id, 'score': score})
+        return self.mongo['filmworks'].update_one(
+            {'_id': filmwork_id},
+            {'$set': {'rating.votes': votes}}
+        )
+
+    async def unrate(self, filmwork_id: UUID | str, user_id: UUID | str):
+        await self.mongo['filmworks'].update_one(
+            {'_id': filmwork_id},
+            {
+                '$pull': {
+                    'rating.votes': {
+                        'user_id': user_id,
+                    },
+                },
+            },
+        )
+
 
 def get_filmworks_service(
         mongo: AsyncIOMotorDatabase = Depends(get_mongo),
