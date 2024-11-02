95beb28c40b0e8f5cb6545dae34b26af28d6335f
MironBerch <mironberchetov@gmail.com>
2024-11-02 20:31:17 +0300

    Create new models for `ugc`

--- images/ugc/src/models/models.py
+++ images/ugc/src/models/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from uuid import UUID
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 
 
 class FilmworkBookmark(BaseModel):
@@ -14,6 +14,10 @@ class Text(BaseModel):
     text: str
 
 
+class Score(BaseModel):
+    score: int
+
+
 class Review(BaseModel):
     """Модель ответа для представления рецензии на фильм."""
 
@@ -22,5 +26,24 @@ class Review(BaseModel):
     filmwork_id: UUID
     text: str
     pub_date: datetime
-    likes: int = Field(default=0)
-    dislikes: int = Field(default=0)
+
+
+class Vote(BaseModel):
+    user_id: UUID
+    score: float = Field(gt=0, le=10)
+
+
+class Rating(BaseModel):
+    average_rating: int | None
+    votes: list[Vote] | None = Field(exclude=True)
+
+    @model_validator(mode='before')
+    @classmethod
+    def scoring(cls, data: dict) -> dict:
+        """
+        Валидатор для подсчета средней пользовательской оценки.
+        """
+        votes: list[Vote] = data.get('votes')
+        if votes:
+            data['average_rating'] = sum([vote.score for vote in votes]) // (len(votes))
+        return data
