31bd2a91fce519edc0476d1b6b3087127100750b
MironBerch <mironberchetov@gmail.com>
2024-11-02 20:40:24 +0300

    Create filmworks handlers for `ugc`

---
+++ images/ugc/src/api/v1/handlers/fimworks.py
@@ -0,0 +1,54 @@
+from uuid import UUID
+
+from fastapi import APIRouter, Depends, Path
+
+from api.dependencies import check_filmwork_exist
+from models.models import Rating, Score
+from services.auth import AuthService
+from services.filmworks import FilmworksService, get_filmworks_service
+
+router = APIRouter(tags=['filmworks'])
+
+
+@router.get(
+    path='/filmworks/{filmwork_id}/ratings',
+    summary='Просмотр рейтинга фильма',
+    response_description='Рейтинг фильма',
+    response_model=Rating,
+    dependencies=[Depends(check_filmwork_exist)],
+)
+async def get_filmwork_rating(
+    service: FilmworksService = Depends(get_filmworks_service),
+    filmwork_id: UUID | str = Path(title='UUID фильма'),
+) -> Rating:
+    return service.get_rating(filmwork_id=filmwork_id)
+
+
+@router.post(
+    path='/filmworks/{filmwork_id}/ratings',
+    summary='Добавление оценки фильму',
+    response_description='Рейтинг фильма',
+    response_model=Rating,
+    dependencies=[Depends(check_filmwork_exist)],
+)
+async def rate_filmwork(
+    score: Score,
+    auth: AuthService = Depends(),
+    service: FilmworksService = Depends(get_filmworks_service),
+    filmwork_id: UUID | str = Path(title='UUID фильма'),
+):
+    return service.rate(filmwork_id=filmwork_id, user_id=auth.user_id, score=score.score)
+
+
+@router.delete(
+    path='/filmworks/{filmwork_id}/ratings',
+    summary='Удаление оценки у фильма',
+    response_description='Результат удаление оценки у фильма',
+    dependencies=[Depends(check_filmwork_exist)],
+)
+async def unrate_filmwork(
+    auth: AuthService = Depends(),
+    service: FilmworksService = Depends(get_filmworks_service),
+    filmwork_id: UUID | str = Path(title='UUID фильма'),
+):
+    return service.unrate(filmwork_id=filmwork_id, user_id=auth.user_id)
