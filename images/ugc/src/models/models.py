7a8f6f7fb9df0357b45a998394ec4023be09db41
MironBerch <mironberchetov@gmail.com>
2024-11-02 21:45:21 +0300

    Update `ugc` models

--- images/ugc/src/models/models.py
+++ images/ugc/src/models/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Literal
 from uuid import UUID
 
 from pydantic import BaseModel, Field, model_validator
@@ -15,7 +16,11 @@ class Text(BaseModel):
 
 
 class Score(BaseModel):
-    score: int
+    score: int = Field(gt=0, le=10)
+
+
+class ReviewScore(BaseModel):
+    score: Literal[1, 10]
 
 
 class Review(BaseModel):
@@ -30,7 +35,7 @@ class Review(BaseModel):
 
 class Vote(BaseModel):
     user_id: UUID
-    score: float = Field(gt=0, le=10)
+    score: int = Field(gt=0, le=10)
 
 
 class Rating(BaseModel):
@@ -46,4 +51,27 @@ class Rating(BaseModel):
         votes: list[Vote] = data.get('votes')
         if votes:
             data['average_rating'] = sum([vote.score for vote in votes]) // (len(votes))
+        return data
+
+
+class ReviewRating(Rating):
+    votes: list[Vote] | None = Field(exclude=True)
+    likes: int = Field(default=0)
+    dislikes: int = Field(default=0)
+    likes_sum: int = Field(default=0)
+
+    @model_validator(mode='before')
+    @classmethod
+    def scoring(cls, data: dict) -> dict:
+        """
+        Валидатор для подсчета лайков и дизлайков.
+        """
+        votes: list[Vote] = data.get('votes')
+        if votes:
+            for vote in votes:
+                if vote.score == 10:
+                    data['likes'] += 1
+                if vote.score == 1:
+                    data['dislikes'] += 1
+            data['likes_sum'] = data['likes'] - data['dislikes']
         return data
