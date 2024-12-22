b757f526d0ea2081fd8bdfbaa741b258e17f9485
MironBerch <mironberchetov@gmail.com>
2024-12-22 23:59:24 +0300

    Create convert binary value to uuid method for `ugc`

--- images/ugc/src/services/utils.py
+++ images/ugc/src/services/utils.py
@@ -6,3 +6,8 @@ from bson.binary import Binary
 def to_binary(value: UUID) -> Binary:
     """Convert `UUID` to MongoDB binary format."""
     return Binary(value.bytes)
+
+
+def to_uuid(value: bytes) -> UUID:
+    """Convert MongoDB binary to `UUID`."""
+    return UUID(bytes=value)
