98db409e27ac20e45fde5b9d032e0ebc8915a98f
MironBerch <mironberchetov@gmail.com>
2024-12-23 18:14:47 +0300

    Update bookmark handlers for `ugc`

--- images/ugc/src/api/v1/handlers/bookmarks.py
+++ images/ugc/src/api/v1/handlers/bookmarks.py
@@ -30,27 +30,29 @@ async def get_bookmarks(
     '/filmworks/{filmwork_id}/bookmarks',
     summary='Добавление фильма в закладки',
     response_description='Результат добавления фильма в закладки',
+    response_model=FilmworkBookmark,
     dependencies=[Depends(check_filmwork_exist)],
 )
 async def bookmark_filmwork(
     auth: AuthService = Depends(),
     service: BookmarksService = Depends(get_bookmarks_service),
     filmwork_id: UUID = Path(title='UUID фильма'),
-) -> dict:
+) -> FilmworkBookmark:
     result = await service.update(user_id=auth.user_id, filmwork_id=filmwork_id)
     return result
 
 
 @router.delete(
-    '/filmworks/{film_id}/bookmarks',
+    '/filmworks/{filmwork_id}/bookmarks',
     summary='Удаление фильма из закладок',
     response_description='Результат удаления фильма из закладок',
+    response_model=FilmworkBookmark,
     dependencies=[Depends(check_filmwork_exist)],
 )
 async def unbookmark_film(
     auth: AuthService = Depends(),
     service: BookmarksService = Depends(get_bookmarks_service),
     filmwork_id: UUID = Path(title='UUID фильма'),
-) -> dict:
+) -> FilmworkBookmark:
     result = await service.remove(user_id=auth.user_id, filmwork_id=filmwork_id)
     return result
