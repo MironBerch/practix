624e7fae364f899aca49d3780bd40e41e8e93756
MironBerch <mironberchetov@gmail.com>
2024-12-24 22:04:01 +0300

    Fix reviews service for `ugc`

--- images/ugc/src/services/reviews.py
+++ images/ugc/src/services/reviews.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from uuid import UUID
+from uuid import UUID, uuid4
 
 from fastapi import Depends
 from motor.motor_asyncio import AsyncIOMotorDatabase
@@ -7,80 +7,110 @@ from motor.motor_asyncio import AsyncIOMotorDatabase
 from api.paginator import Paginator
 from db.mongo import get_mongo
 from services.base import BaseService
-from services.utils import to_binary
+from services.utils import to_binary, to_uuid
 
 
 class ReviewsService(BaseService):
     async def get(self, user_id: UUID, filmwork_id: UUID):
         result = await self.mongo['reviews'].find_one(
-            {'filmwork_id': to_binary(filmwork_id), 'author': to_binary(user_id)},
+            {'filmwork_id': to_binary(filmwork_id), 'author_id': to_binary(user_id)},
         )
         return result
 
     async def filter(self, filmwork_id: UUID, paginator: Paginator):
         result = (
             await self.mongo['reviews']
-            .find({'filmwork_id': filmwork_id})
+            .find({'filmwork_id': to_binary(filmwork_id)})
             .skip((paginator.page - 1) * paginator.size)
             .limit(paginator.size)
             .to_list(length=None)
         )
+        for obj in result:
+            obj['id'] = to_uuid(obj['_id'])
+            obj['filmwork_id'] = to_uuid(obj['filmwork_id'])
+            obj['author_id'] = to_uuid(obj['author_id'])
         return result
 
     async def update(self, user_id: UUID, filmwork_id: UUID, text: str):
-        result = await self.mongo['reviews'].update_one(
+        object_id = to_binary(uuid4())
+        await self.mongo['reviews'].update_one(
             {
-                'author': to_binary(user_id),
+                'author_id': to_binary(user_id),
                 'filmwork_id': to_binary(filmwork_id),
             },
             {
-                '$set': {
-                    'text': text,
-                    'pub_date': datetime.now(),
-                },
+                '$set': {'text': text},
                 '$setOnInsert': {
-                    'text': text,
-                }
+                    '_id': object_id,
+                    'pub_date': datetime.now(),
+                    'rating': {'votes': []},
+                    'author_id': to_binary(user_id),
+                    'filmwork_id': to_binary(filmwork_id),
+                },
             },
             upsert=True,
         )
-        return result
-
-    async def remove(self, user_id: UUID, filmwork_id: UUID):
-        result = await self.mongo['reviews'].delete_one(
-            {
-                'author': to_binary(user_id),
-                'filmwork_id': to_binary(filmwork_id),
-            }
+        result = await self.mongo['reviews'].find_one(
+            {'filmwork_id': to_binary(filmwork_id), 'author_id': to_binary(user_id)},
         )
+        result['id'] = to_uuid(result['_id'])
+        result['filmwork_id'] = to_uuid(result['filmwork_id'])
+        result['author_id'] = to_uuid(result['author_id'])
         return result
 
-    async def get_rating(self, review_id: UUID):
-        result = await self.mongo['reviews'].find_one({'_id': to_binary(review_id)})
+    async def remove(self, review_id: UUID, user_id: UUID, filmwork_id: UUID):
+        await self.mongo['reviews'].delete_one(
+            {
+                '_id': to_binary(review_id),
+                'author_id': to_binary(user_id),
+                'filmwork_id': to_binary(filmwork_id),
+            },
+        )
+
+    async def get_rating(self, review_id: UUID, filmwork_id: UUID):
+        result = await self.mongo['reviews'].find_one(
+            {
+                '_id': to_binary(review_id),
+                'filmwork_id': to_binary(filmwork_id),
+            },
+        )
         return result['rating']
 
-    async def rate(self, review_id: UUID, user_id: UUID, score: int):
-        review_filter = {'_id': to_binary(review_id)}
-        review: dict = await self.mongo['reviews'].find_one(review_filter)
+    async def rate(self, review_id: UUID, user_id: UUID, filmwork_id: UUID, score: int):
+        user_id_bytes = user_id.bytes
+        review: dict = await self.mongo['reviews'].find_one({'_id': to_binary(review_id)})
         votes: list = review.get('rating', {}).get('votes', [])
         for vote in votes:
-            if vote['user_id'] == user_id:
+            if vote['user_id'] == user_id_bytes:
                 vote['score'] = score
                 break
         else:
-            votes.append({'user_id': user_id, 'score': score})
+            votes.append({'user_id': to_binary(user_id), 'score': score})
         result = await self.mongo['reviews'].update_one(
             {'_id': to_binary(review_id)},
             {'$set': {'rating.votes': votes}},
+            upsert=True,
         )
-        return result
+        result = await self.mongo['reviews'].find_one(
+            {
+                '_id': to_binary(review_id),
+                'filmwork_id': to_binary(filmwork_id),
+            },
+        )
+        return result['rating']
 
-    async def unrate(self, review_id: UUID, user_id: UUID):
-        result = await self.mongo['reviews'].update_one(
+    async def unrate(self, review_id: UUID, filmwork_id: UUID, user_id: UUID):
+        await self.mongo['reviews'].update_one(
             {'_id': to_binary(review_id)},
-            {'$pull': {'rating.votes': {'user_id': user_id}}},
+            {'$pull': {'rating.votes': {'user_id': to_binary(user_id)}}},
         )
-        return result
+        result = await self.mongo['reviews'].find_one(
+            {
+                '_id': to_binary(review_id),
+                'filmwork_id': to_binary(filmwork_id),
+            },
+        )
+        return result['rating']
 
 
 def get_reviews_service(
