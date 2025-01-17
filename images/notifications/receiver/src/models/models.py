6e98ff47a054e325f438ffdffda9114b5ab97967
MironBerch <mironberchetov@gmail.com>
2025-01-17 20:41:45 +0300

    Add base models for `notifications` receiver

---
+++ images/notifications/receiver/src/models/models.py
@@ -0,0 +1,12 @@
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+
+class CustomBaseModel(BaseModel):
+    class Config:
+        populate_by_name = True
+
+
+class UUIDMixin(CustomBaseModel):
+    id: UUID = Field(alias='uuid')
