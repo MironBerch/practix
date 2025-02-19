a2aebf82616988eaa861ba5e6d0a47870d9da26d
MironBerch <mironberchetov@gmail.com>
2025-02-20 02:53:09 +0300

    Create main terraform file for YC infrastructure

---
+++ terraform/infra/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    yandex = {
+        source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    bucket = "practix-bucket"
+    region = "ru-central1-d"
+    key    = "terraform/state/terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
+}
+
+provider "yandex" {
+  token                    = var.token
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = "ru-central1-d"
+}
