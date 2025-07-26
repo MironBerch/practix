8c4158b9ee41b2c2753c54fd380dd5b1597db038
MironBerch <mironberchetov@gmail.com>
2025-07-26 15:01:46 +0300

    Add header for `frontend`

--- images/frontend/src/App.vue
+++ images/frontend/src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
   import MainPage from './pages/MainPage.vue';
+  import Header from './components/Header.vue';
 </script>
 
 <template>
+  <Header />
   <MainPage />
 </template>
 
---
+++ images/frontend/src/components/Header.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h2>PRACTIX</h2>
+    <ul>
+      <li>
+        <a href="/" class="">Home</a>
+      </li>
+      <li>
+        <a href="/" class="">TV Shows</a>
+      </li>
+      <li>
+        <a href="/" class="">Movies</a>
+      </li>
+      <li>
+        <a href="/" class="">Latest</a>
+      </li>
+      <li>
+        <a href="/" class="">My List</a>
+      </li>
+    </ul>
+    <form>
+      <input>
+      <button type="button"></button>
+    </form>
+    <div>
+      notification icon
+    </div>
+    <div>
+      user icon
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
